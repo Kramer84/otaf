ac414b7ceac17335bc2b26bb9cb783a981007b3b
Kramer84 <kristof.attila.private@gmail.com>
2026-05-04 03:41:53 +0200

    chore: sync 2026-05-04 03:41:53+0200 Kramer84@motherloadubu

--- src/otaf/example_models/models_3_D/__init__.py
+++ src/otaf/example_models/models_3_D/__init__.py
@@ -4,10 +4,20 @@ from __future__ import annotations
 __author__ = "Kramer84"
 
 from ._model_8dof import *
+from ._model_4_pins_manual import *
 
 # You must explicitly define __all__ here for the parent to access it
 __all__ = ["SDA_compatibility_loops_expanded",
     "CLH_get_compatibility_expression_from_FO_matrices",
     "getAssemblyDataProcessorObject",
     "getCompatibilityLoopHandlingObject",
-    "getInterfaceLoopHandlingObject"]
+    "getInterfaceLoopHandlingObject",
+    "print_interface_constraints",
+    "get_kinematic_loop_expressions",
+    "get_interface_constraint_expressions",
+    "create_dynamic_mapping",
+    "rename_variables",
+    "rename_variables",
+    "print_kinematic_loops",
+    "defect_vars",
+    "clearance_vars"]
